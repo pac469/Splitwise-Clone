cbf0849c424ab869e40e5620e50e9ef2717f27c9
Paulin Alcoser <50881828+pac469@users.noreply.github.com>
2022-03-13 13:36:20 +0400

    Only authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,27 +8,13 @@ import '@aws-amplify/ui-react/styles.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Typescript push
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Authenticator socialProviders={['amazon', 'apple', 'facebook', 'google']}>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
+        {({ signOut, user }) => (
+          <main>
+            <h1>Hello {user.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
       </Authenticator>
     </div>
   );

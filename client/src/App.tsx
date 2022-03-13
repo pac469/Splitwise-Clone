fe632407724cbbe2ffe402170a6d96340e821a65
Paulin Alcoser <50881828+pac469@users.noreply.github.com>
2022-03-13 13:35:20 +0400

    Added authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
@@ -20,9 +22,16 @@ function App() {
           Learn React
         </a>
       </header>
-      <AmplifySignOut />
+      <Authenticator socialProviders={['amazon', 'apple', 'facebook', 'google']}>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+      </Authenticator>
     </div>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
